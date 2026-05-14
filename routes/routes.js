ea9688d04409b5f1f50c6a2432622bc746b6298f
Yolanda Wang <mynameisyolandawang@gmail.com>
2026-05-14 10:59:36 -0700

    Back button in events is working and admin logout button is working

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { loadPage, addPoint, edit, pointPage, del, pointHistory} from '../controllers/pointControllers.js';
 import { loadEvents, adminEvents, addEvent, deleteEvent, studentEvents} from '../controllers/eventControllers.js';
-import { adminEnter, editEnter, login } from '../controllers/adminControllers.js';
+import { adminEnter, adminLoad, editEnter, login } from '../controllers/adminControllers.js';
 import { requireAuth } from '../middleware/auth.js';
 
 export const router = express.Router();
@@ -20,17 +20,15 @@ router.post('/deleteEvent/:id', deleteEvent);
 //admin login pages
 router.get("/login", login);
 router.post('/adminEnter', adminEnter);
+router.get('/adminLoad', adminLoad);
 router.get('/editEnter', requireAuth, editEnter);
-//admin pages
-router.post("/addPoint", addPoint);
-router.get('/adminEnter', adminEnter);
-router.get('/editEnter', editEnter);
+
 
 //Point routes
 //edit does this need to exist?
 router.get('/:id/edit', edit);
 router.post('/:id', pointPage);
 
-//delete
+router.post("/addPoint", addPoint);
 router.post('/:id/delete', del);
 router.get('/pointHistory', pointHistory);
