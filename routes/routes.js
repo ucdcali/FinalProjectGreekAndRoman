e08c1291c3a32fe3f08eee56563408bfde696728
Yolanda Wang <mynameisyolandawang@gmail.com>
2026-05-14 10:50:19 -0700

    working point history

--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,36 @@
 import express from 'express';
 
-import { loadPage, addPoint, edit, pointPage, del } from '../controllers/pointControllers.js';
+import { loadPage, addPoint, edit, pointPage, del, pointHistory} from '../controllers/pointControllers.js';
 import { loadEvents, adminEvents, addEvent, deleteEvent, studentEvents} from '../controllers/eventControllers.js';
 import { adminEnter, editEnter, login } from '../controllers/adminControllers.js';
 import { requireAuth } from '../middleware/auth.js';
 
 export const router = express.Router();
 
-//write a route
+//general studen page load
 router.get("/", loadPage);
 
-router.post("/addPoint", addPoint);
-router.get('/adminEnter', adminEnter);
-router.get('/editEnter', editEnter);
+//event pages
 router.get('/events', loadEvents);
 router.get('/adminEvents', adminEvents);
 router.get('/studentEvents', studentEvents);
 router.post('/addEvents', addEvent);
 router.post('/deleteEvent/:id', deleteEvent);
 
+//admin login pages
 router.get("/login", login);
 router.post('/adminEnter', adminEnter);
 router.get('/editEnter', requireAuth, editEnter);
-//edit
+//admin pages
+router.post("/addPoint", addPoint);
+router.get('/adminEnter', adminEnter);
+router.get('/editEnter', editEnter);
+
+//Point routes
+//edit does this need to exist?
 router.get('/:id/edit', edit);
 router.post('/:id', pointPage);
+
 //delete
 router.post('/:id/delete', del);
+router.get('/pointHistory', pointHistory);
